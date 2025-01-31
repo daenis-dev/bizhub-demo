6454944243d50741a55d710a40f66dfd5916f437
daenis-dev <daenis.dev@gmail.com>
2025-01-30 17:49:54 -1000

    Second schema update - front end still outdated

--- checkers-api/sql/schema.sql
+++ checkers-api/sql/schema.sql
@@ -2,8 +2,8 @@ CREATE SEQUENCE IF NOT EXISTS backups_id_seq;
 
 CREATE TABLE backups (
 	id INT NOT NULL DEFAULT nextval('backups_id_seq') PRIMARY KEY,
-	name VARCHAR(255) NOT NULL,
-    file_name VARCHAR(255) NOT NULL,
+    file_path VARCHAR(255) NOT NULL,
+    file_extension VARCHAR(7) NOT NULL,
     is_compressed BOOLEAN NOT NULL,
     user_id VARCHAR(255) NOT NULL
 );
